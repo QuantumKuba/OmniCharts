0a9835d8a5298d90a0382a65de3ac732c0d6c179
kubakakauko <47721520+kubakakauko@users.noreply.github.com>
2024-11-17 15:04:12 +0000

    backup

--- src/App.svelte
+++ src/App.svelte
@@ -2,43 +2,42 @@
 <script>
     import math from "./stuff/math.js";
 
-    import.meta.hot
-    import {NightVision} from './index.js'
-    import {onMount} from 'svelte'
-    // import data from '../data/data-ohlcv.json?id=main'
-    import data from '../data/data-ohlcv-rsi.json?id=main'
-    import data2 from '../data/data-area.json?id=main-2'
+    import.meta.hot;
+    import { NightVision } from "./index.js";
+    import { onMount } from "svelte";
+    // import data from "../data/data-ohlcv.json?id=main";
+    import data from "../data/data-ohlcv-rsi.json?id=main";
+    import data2 from "../data/data-area.json?id=main-2";
     // import data3 from '../data/data-aapl.json?id=main-3'
-    import TestStack from '../tests/testStack.js'
-
+    import TestStack from "../tests/testStack.js";
 
     // Tests
-    import fullReset from '../tests/data-sync/fullReset.js'
-    import paneAddRem from '../tests/data-sync/paneAddRem.js'
-    import paneSettings from '../tests/data-sync/paneSettings.js'
-    import ovAddRem from '../tests/data-sync/ovAddRem.js'
-    import scaleChange from '../tests/data-sync/scaleChange.js'
-    import mainOverlay from '../tests/data-sync/mainOverlay.js'
-    import ovSettings from '../tests/data-sync/ovSettings.js'
-    import ovPropsChange from '../tests/data-sync/ovPropsChange.js'
-    import ovDataChange from '../tests/data-sync/ovDataChange.js'
-    import Math2 from './stuff/math.js';
+    import fullReset from "../tests/data-sync/fullReset.js";
+    import paneAddRem from "../tests/data-sync/paneAddRem.js";
+    import paneSettings from "../tests/data-sync/paneSettings.js";
+    import ovAddRem from "../tests/data-sync/ovAddRem.js";
+    import scaleChange from "../tests/data-sync/scaleChange.js";
+    import mainOverlay from "../tests/data-sync/mainOverlay.js";
+    import ovSettings from "../tests/data-sync/ovSettings.js";
+    import ovPropsChange from "../tests/data-sync/ovPropsChange.js";
+    import ovDataChange from "../tests/data-sync/ovDataChange.js";
+    import Math2 from "./stuff/math.js";
 
     // More tests
-    import realTime from '../tests/real-time/realTime.js'
+    import realTime from "../tests/real-time/realTime.js";
 
     // More tests
-    import timeBased from '../tests/tfs-test/allTimeBased.js'
-    import indexBased from '../tests/tfs-test/allIndexBased.js'
+    import timeBased from "../tests/tfs-test/allTimeBased.js";
+    import indexBased from "../tests/tfs-test/allIndexBased.js";
 
     // More tests
-    import indicators from '../tests/indicators/indicators.js'
-    import rangeTool from '../tests/tools/rangeTool.js'
-    import lineTool from '../tests/tools/lineTool.js'
-    import watchPropTest from '../tests/navy/watchPropTest.js'
+    import indicators from "../tests/indicators/indicators.js";
+    import rangeTool from "../tests/tools/rangeTool.js";
+    import lineTool from "../tests/tools/lineTool.js";
+    import watchPropTest from "../tests/navy/watchPropTest.js";
 
     // More tests
-    import logScaleTest from '../tests/scales/logScale.js'
+    import logScaleTest from "../tests/scales/logScale.js";
 
     /*
     TODO: data-api interface:
@@ -51,85 +50,88 @@
 
     // TODO: Memory leak tests
 
-    let stack = new TestStack()
-    let chart = null
+    let stack = new TestStack();
+    let chart = null;
 
     //data.indexBased = true
 
     onMount(() => {
-        chart = new NightVision('chart-container', {
+        chart = new NightVision("chart-container", {
             data: data,
             timezone: new Date().getTimezoneOffset() / -60,
             autoResize: true,
             indexBased: true,
             scrollLock: true,
-            id: 'test',
+            id: "test",
             config: {
-                ZOOM_MODE: 'tl',
-                SCROLL_WHEEL: 'prevent',
+                ZOOM_MODE: "tl",
+                SCROLL_WHEEL: "prevent",
                 scrollLock: true,
                 SBMAX: 500,
                 DEFAULT_LEN: 150,
                 meta: {
-                    scrollLock: true
-                }
+                    scrollLock: true,
+                },
             },
             meta: {
-                scrollLock: true
+                scrollLock: true,
             },
-        })
+        });
 
-        chart.meta.initHeatmap('test');
+        chart.meta.initHeatmap("test");
 
         // chart.meta.scrollLock = true;
         // chart.data = data3
-        window.chart = chart
-        window.stack = stack
+        window.chart = chart;
+        window.stack = stack;
 
-        stack.setGroup('data-sync')
+        stack.setGroup("data-sync");
 
-        fullReset(stack, chart)
-        paneAddRem(stack, chart)
-        paneSettings(stack, chart)
-        ovAddRem(stack, chart)
-        scaleChange(stack, chart)
-        mainOverlay(stack, chart)
-        ovSettings(stack, chart)
-        ovPropsChange(stack, chart)
-        ovDataChange(stack, chart)
+        fullReset(stack, chart);
+        paneAddRem(stack, chart);
+        paneSettings(stack, chart);
+        ovAddRem(stack, chart);
+        scaleChange(stack, chart);
+        mainOverlay(stack, chart);
+        ovSettings(stack, chart);
+        ovPropsChange(stack, chart);
+        ovDataChange(stack, chart);
 
-        stack.setGroup('real-time')
+        stack.setGroup("real-time");
 
-        realTime(stack, chart)
+        realTime(stack, chart);
 
-        stack.setGroup('tfs-test')
+        stack.setGroup("tfs-test");
 
-        timeBased(stack, chart)
-        indexBased(stack, chart)
+        timeBased(stack, chart);
+        indexBased(stack, chart);
 
-        stack.setGroup('ind-test')
+        stack.setGroup("ind-test");
 
-        indicators(stack, chart)
+        indicators(stack, chart);
 
-        stack.setGroup('tools-test')
+        stack.setGroup("tools-test");
 
-        rangeTool(stack, chart)
-        lineTool(stack, chart)
+        rangeTool(stack, chart);
+        lineTool(stack, chart);
 
-        stack.setGroup('navy-test')
+        stack.setGroup("navy-test");
 
-        watchPropTest(stack, chart)
+        watchPropTest(stack, chart);
 
-        stack.setGroup('scales-test')
+        stack.setGroup("scales-test");
 
-        logScaleTest(stack, chart)
+        logScaleTest(stack, chart);
 
         //  Type in the console: stack.execAll()
         //  or: stack.exec('<group>')
-
-    })
-
+    });
 </script>
+
+<div class="app">
+    <div id="chart-container"></div>
+</div>
+
 <style>
     .app {
         width: 1080px;
@@ -145,6 +147,3 @@
         height: 100%;
     }
 </style>
-<div class="app">
-    <div id="chart-container"></div>
-</div>
